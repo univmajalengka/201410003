2eb3053c0adb00ed2b8f8becc091540363329317
viniarifianir16 <viniarifianir16@gmail.com>
2020-12-02 06:21:17 +0700

    Tugas #5

---
+++ tugas_algoritma_pemrograman/201410003_ViniArifianiRohmat_Tgs5-1.cpp
@@ -0,0 +1,19 @@
+// Konversi jam, menit dan detik ke total detik
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int j,m,d,td;
+	cout<<"Konversi jam, menit dan detik ke total detik"<<endl;
+	cout<<"--------------------------------------------"<<endl;
+	cout<<"Masukan Jam : ";
+	cin>>j;
+	cout<<"Masukan Menit : ";
+	cin>>m;
+	cout<<"Masukan Detik : ";
+	cin>>d;
+	td = (j*3600)+(m*60)+d;
+	cout<<"Total Detik : "<<td;
+	return 0;
+}
